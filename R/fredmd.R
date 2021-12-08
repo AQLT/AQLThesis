f2bf6b7edc9a3e267bb9a20955c2574ab7c54b0f
AQlT <alain.quartier@yahoo.fr>
2021-12-08 17:12:46 +0100

    fredmd documentation + corrections as ts objects

--- R/fredmd.R
+++ R/fredmd.R
@@ -1,6 +1,12 @@
+#' Import fredmd database
+#'
+#' @param file path to a file.
+#' @param date_start,date_end optional date start and end.
+#' @param transform boolean indicating if the data should be transformed.
 #'@export
 #'@importFrom readr read_csv cols col_date
 #'@importFrom utils read.csv
+#'@importFrom stats lag
 fredmd <- function (file = "", date_start = NULL, date_end = NULL, transform = TRUE)
 {
   if (!is.logical(transform))
@@ -26,66 +32,63 @@ fredmd <- function (file = "", date_start = NULL, date_end = NULL, transform = T
   nom_col <- colnames(read.csv(file,header = TRUE,nrows =1))
   header <- c("date", nom_col[-1])
   colnames(rawdata) <- header
-  tcode <- read.csv(file,header = FALSE,nrows =1,skip = 1,)[1,-1]
-  transxf <- function(x, tcode) {
-    n <- length(x)
-    small <- 1e-06
-    y <- rep(NA, n)
-    y1 <- rep(NA, n)
-    if (tcode == 1) {
-      y <- x
-    }
-    else if (tcode == 2) {
-      y[2:n] <- x[2:n] - x[1:(n - 1)]
-    }
-    else if (tcode == 3) {
-      y[3:n] <- x[3:n] - 2 * x[2:(n - 1)] + x[1:(n - 2)]
-    }
-    else if (tcode == 4) {
-      if (min(x, na.rm = TRUE) > small)
-        y <- log(x)
-    }
-    else if (tcode == 5) {
-      if (min(x, na.rm = TRUE) > small) {
-        x <- log(x)
-        y[2:n] <- x[2:n] - x[1:(n - 1)]
-      }
-    }
-    else if (tcode == 6) {
-      if (min(x, na.rm = TRUE) > small) {
-        x <- log(x)
-        y[3:n] <- x[3:n] - 2 * x[2:(n - 1)] + x[1:(n -
-                                                     2)]
-      }
-    }
-    else if (tcode == 7) {
-      y1[2:n] <- (x[2:n] - x[1:(n - 1)])/x[1:(n - 1)]
-      y[3:n] <- y1[3:n] - y1[2:(n - 1)]
-    }
-    return(y)
-  }
-  if (transform) {
-    N <- ncol(rawdata)
-    data <- rawdata
-    data[, 2:N] <- NA
-    for (i in 2:N) {
-      temp <- transxf(rawdata[, i], tcode[i - 1])
-      data[, i] <- temp
-    }
-  }else {
-    data <- rawdata
-  }
+  tcode <- read.csv(file,header = FALSE,nrows =1,skip = 1)[1,-1]
+
+  data <- rawdata
+
   if (is.null(date_start))
-    date_start <- as.Date("1959-01-01")
+    date_start <- data[1,1]
   if (is.null(date_end))
     date_end <- data[, 1][nrow(data)]
   index_start <- which.max(data[, 1] == date_start)
   index_end <- which.max(data[, 1] == date_end)
-  outdata <- data[index_start:index_end, ]
-  class(outdata) <- c("data.frame", "fredmd")
-  first_date <- as.character(outdata[1,1])
+  data <- data[index_start:index_end, ]
+  first_date <- as.character(data[1,1])
 
-  return(ts(outdata[,-1],
-            start = as.numeric(c(substr(first_date,1,4), substr(first_date,6,7))),
-            frequency = 12))
+
+  ts_data <- ts(data[,-1],
+                start = as.numeric(c(substr(first_date,1,4), substr(first_date,6,7))),
+                frequency = 12)
+
+
+  if (transform) {
+    data_out <- do.call(cbind, lapply(seq_len(ncol(ts_data)),function(i){
+      transxfred(ts_data[,i], tcode[i])
+    }))
+    colnames(data_out) <- colnames(ts_data)
+    ts_data <- data_out
+  }
+
+  return(ts_data)
+}
+
+transxfred <- function(x, tcode) {
+  small <- 1e-06
+  y <- NULL
+  if (tcode == 1) {
+    y <- x
+  }else if (tcode == 2) {
+    y <- diff(x)
+  }else if (tcode == 3) {
+    y <- diff(diff(x))
+  }else if (tcode == 4) {
+    if (min(x, na.rm = TRUE) > small)
+      y <- log(x)
+  }else if (tcode == 5) {
+    if (min(x, na.rm = TRUE) > small) {
+      x <- log(x)
+      y <- diff(x)
+    }
+  }else if (tcode == 6) {
+    if (min(x, na.rm = TRUE) > small) {
+      x <- log(x)
+      y <- diff(diff(x))
+    }
+  }else if (tcode == 7) {
+    y1 <- diff(x)/stats::lag(x)
+    y <- diff(y1)
+  }else{
+    stop("wrong tcode")
+  }
+  return(y)
 }
