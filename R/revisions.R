ea4e31d35113aecb6e923b91e4039cccd7ebadb2
AQlT <alain.quartier@yahoo.fr>
2022-01-21 10:17:04 +0100

    summary revision

--- R/revisions.R
+++ R/revisions.R
@@ -34,3 +34,42 @@ consecutive_est_revisions <- function(x){
 
   revisions_consest
 }
+
+
+#'@export
+summary_revisions <- function(data,
+                              peaks = nber_tp_m[,"Peak"],
+                              troughs = nber_tp_m[,"Trough"],
+                              annual_frequency_tp = 2){
+  tp = c(na.omit(peaks),na.omit(troughs))
+  data = data.frame(data)
+  data$date = as.numeric(rownames(data))
+  data$type = "normal"
+  data$total = "total"
+  for(date in tp){
+    data$type[(data$date <= (date + 1/annual_frequency_tp)) &
+                (data$date >= (date - 1/annual_frequency_tp))] <- "turning-point"
+  }
+  rmse <- function(x, ...){
+    sqrt(mean(x^2, na.rm = TRUE, ...))
+  }
+  mae = function(x, ...){
+    mean(x, na.rm=TRUE, ...)
+  }
+
+  global_stats = NULL
+  for(FUN in c("rmse", "mae")){
+    stats = apply(data[,1:11],2, FUN)
+    stats_group = aggregate(data[,1:11], by = list(data$type), FUN)
+    colnames(stats_group)[1] <- "Group"
+    stats = rbind(stats_group,
+                  data.frame("Group" = "total", t(stats)))
+    stats$stats = toupper(FUN)
+    if(is.null(global_stats)){
+      global_stats <- stats
+    } else {
+      global_stats <- rbind(global_stats, stats)
+    }
+  }
+  global_stats
+}
